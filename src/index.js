f7d3e49fccf49b7f439247f1d597be3be2f2a3c5
latinawhocodes <alyssaroque01@gmail.com>
2019-10-17 16:40:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
-const url = 'http://localhost:3000/toys'
-let addToy = false
-let divToy = document.querySelector('#toy-collection')
-
+const toyList = document.querySelector('div#toy-collection')
+const toyUrl = 'http://localhost:3000/toys'
 // YOUR CODE HERE
-function getToys(){
-  return fetch(url)
-  .then(response => response.json())
-  .then(allToys = > processToys(allToys))
-}
 
-function processToys(allToys){
-  allToys.forEach(toy => toyCards(toy))
-}
+const toyName = document.querySelector('input[name="name"]')
+const toyImage = document.querySelector('input[name="image"]')
 
-function postToy(aboutToy){
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-
-    body: JSON.stringify({
-      "name": aboutToy.name.value,
-      "image": aboutToy.image,
-      "likes": 0
-    })
+function postToy(info){
+  let toy = {
+    name: toyName.value,
+    image: toyImage.value,
+    likes: 0
   }
+
+  fetch(url, {
+    method: 'POST',
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+
+    body: JSON.stringify(toy)
+  })
+
   .then(response => response.json())
-  .then(objToy) => {
-    let newToy = toyCards(objToy)
-    divToy.append(newToy)
+  .then(likeObj => {
+    event.target
   })
 }
 
