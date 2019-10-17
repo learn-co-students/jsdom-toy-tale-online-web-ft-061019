936cf335ceeb3f08a9244f43dbc98d9d292f9634
latinawhocodes <alyssaroque01@gmail.com>
2019-10-17 17:29:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+
 const toyList = document.querySelector('div#toy-collection')
 const toyUrl = 'http://localhost:3000/toys'
-// YOUR CODE HERE
-
 const toyName = document.querySelector('input[name="name"]')
 const toyImage = document.querySelector('input[name="image"]')
+let toys = [ ]
 
 function postToy(info){
   let toy = {
@@ -16,19 +16,21 @@ function postToy(info){
 
   fetch(url, {
     method: 'POST',
+    headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
-
+    },
     body: JSON.stringify(toy)
   })
 
   .then(response => response.json())
-  .then(likeObj => {
-    event.target
+  .then(toy => {
+    toyList.push(toy);
+    displayToys();
   })
 }
 
-
+toyForm.addEventListener('submit', addToy)
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
@@ -38,6 +40,62 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
+document.addEventListener('DOMContentLoaded', function(){
+  fetch(url)
+  .then(response => response.json())
+  .then(addToy)
+})
+
+function addToys(data){
+  toys = data;
+  displayToys();
+}
+
+function displayToys(){
+  toyList.innerHTML = " "
+  toys.forEach(toy => formatElements(toy))
+}
+
+function formatElements(toy){
+  let div = document.createElement('div');
+  let h2 = document.createElement('h2');
+  let img = document.createElement('img');
+  let p = document.createElement('p');
+  let button = document.createElement('button');
+
+  div.className = 'card';
+  h2.textContent = toy.name;
+  p.textContent = toy.likes;
+
+  img.src = toy.image;
+  img.className = "toy-avatar";
+
+  button.innerText = "Like";
+  button.className = "like-btn";
+  button.id = toy.id;
+  button.addEventListener('click', incrementLikes);
+
+  div.appendChild(h2);
+  div.appendChild(img);
+  div.appendChild(p);
+  div.appendChild(button);
+
+  toyList.appendChild(div);
+}
 
 
-// OR HERE!
+function incrementLikes(toy){
+  let toy = toys.find(toy => toy.id === id);
+  toy.likes += 1;
+
+  fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({likes: toy.likes})
+  })
+  .then(response => response.json)
+  .then(json => displayToys)
+}
